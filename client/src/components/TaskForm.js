f9f3d7303187e13acfcce0916a9d681f37a1f2b3
Student-Raso <a20311079@uthermosillo.edu.mx>
2022-01-08 17:58:55 -0700

    se crearon funciones handler submit, handleSubmit y useState para preparar datos en taskForm

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,7 +1,94 @@
+import {
+    Button, 
+    Card, 
+    CardContent, 
+    Grid, 
+    TextField, 
+    Typography
+} from '@mui/material';
+import {useState, useEffect} from 'react'
+
 export default function TaskForm() {
+
+    const [task, setTask] = useState({
+        title: '',
+        description: '',
+    })
+    
+    //se guarda el input al hacer submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(task);
+    }
+
+    //se captura input de campos title y description en un evento y se imprime en consola
+    const handleChange = (e) => 
+        setTask({...task, [e.target.name]: e.target.value});
+    
     return (
-        <div>
-            Task Form
-        </div>
+        <Grid 
+        container 
+        direction='column' 
+        alignItems='center'
+        justifyContent='center'
+        >
+            <Grid item xs={3}>
+                <Card sx={{mt: 5}}
+                    style={{
+                        backgroundColor: '#1e272e',
+                        padding: '1rem'
+                    }}>
+                    <Typography
+                        variant='5'
+                        textAlign='center'
+                        color='white'
+                    >
+                        Create Task
+                    </Typography>
+                    <CardContent>
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                variant='filled'
+                                label='Write your title'
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+
+                                name="title"
+                                onChange={handleChange}
+                                inputProps={{ style: { color: "white" } }}
+                                InputLabelProps={{ style: { color: "white" } }}
+                            />
+
+                            <TextField
+                                variant='filled'
+                                label='Write your description'
+                                multiline
+                                rows={4}
+                                sx={{
+                                    display: 'block',
+                                    margin: '.5rem 0'
+                                }}
+
+                                name="description" 
+                                onChange={handleChange}                               
+                                inputProps={{ style: { color: "white" } }}
+                                InputLabelProps={{ style: { color: "white" } }}
+                            />
+
+                            <Button 
+                            variant='contained'
+                            color='primary'
+                            type='submit'>
+                                Save
+                            </Button>
+
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
     )
 }
