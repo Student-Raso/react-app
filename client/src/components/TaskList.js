ab862545dd5b69acba8b4b803e94f91e6f4aee7b
Student-Raso <a20311079@uthermosillo.edu.mx>
2022-01-10 11:50:12 -0700

    task list ready

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,7 +1,52 @@
+import {useEffect, useState} from 'react'
+import {Button, Card, CardContent, Typography} from '@mui/material'
+
 export default function TaskList() {
+    const [tasks, setTasks] = useState([])
+
+    const loadTasks = async () => {
+        const response = await fetch('http://localhost:4000/tasks')
+        const data = await response.json()
+        setTasks(data)
+    }
+    
+    useEffect(() => {
+        loadTasks()
+    }, []);
+
     return (
-        <div>
-            Task List
-        </div>
+        <>
+            <h1>Task List</h1>
+            {tasks.map((task) =>(
+                    <Card style={{
+                        marginBottom: ".7rem",
+                        backgroundColor: "#dcdcfc"}}>
+                        <CardContent style={{
+                            display: "flex",
+                            justifyContent: "space-between"
+                        }}>
+                        <div>
+                            <Typography>{task.title}</Typography>
+                            <Typography>{task.description}</Typography>
+                        </div>
+                        <div>
+                            <Button variant='contained' color='inherit'
+                                onClick={() => console.log("edit")}>
+                                Edit
+                            </Button>
+                            <Button variant='contained' color='warning'
+                                onClick={() => console.log("delete")}
+                                style={{marginLeft: ".5rem"}}>
+                                Delete
+                            </Button>
+                        </div>
+                        </CardContent>
+                    </Card>
+            ))}
+        </>
     )
 }
+
+    
+
+    
